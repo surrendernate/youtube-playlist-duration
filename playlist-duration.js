0996ce14c6f6046b6b5185355c14a4885d87b33b
surrendernate <surrendernate@gmail.com>
2016-08-23 21:57:55 -0400

    Refactored to be more efficient.

--- playlist-duration.js
+++ playlist-duration.js
@@ -1,17 +1,16 @@
 // Look for changes in title tag because page is ajax and no document ready occurs.
-$('title').bind('DOMSubtreeModified', LoadTimeOut);
+$('.load-more-button').bind('click', LoadTimeOut);
+
 // This calls when page is reloaded and document is ready because above does not work.
 PlayListDuration();
-
 function LoadTimeOut(){
-    setTimeout(PlayListDuration, 50);
+    setTimeout(PlayListDuration, 3000);
 }
 function PlayListDuration() {
     // Add a Duration list item in playlist details.
     $('.pl-header-details').append("<li id='duration'></li>");
     var count;
     var time = [];
-    var timestamp;
     var hours = 0;
     var minutes = 0;
     var seconds = 0;
@@ -19,33 +18,21 @@ function PlayListDuration() {
     $('.timestamp span').each(function() {
         // Look for each video in playlist and get .timetap string
         // then count how many ':' exits.
+        // convert the string "numbers" stores in time array to Numbers
         count = ($(this).text().match(/:/g)).length;
+        time = $(this).text().split(':');
+        time = time.map(Number);
 
         // if count is 2 use the hh:mm:ss format else use mm:ss format,
-        // convert the string "numbers" stores in time array to Numbers
-        // then store each time value in corresponding variables from timsstamp object.
+        // then store each time value in corresponding variables hour, minutes, seconds.
         if (count == 2) {
-            time = $(this).text().split(':');
-            time = time.map(Number);
-            timestamp = {
-                hours: time[0],
-                minutes: time[1],
-                seconds: time[2]
-            };
-            seconds = seconds + timestamp.seconds;
-            minutes = minutes + timestamp.minutes;
-            hours = hours + timestamp.hours;
+            hours   += time[0];
+            minutes += time[1];
+            seconds += time[2];
         } else {
-            time = $(this).text().split(':');
-            time = time.map(Number);
-            timestamp = {
-                minutes: time[0],
-                seconds: time[1]
-            };
-            seconds = seconds + timestamp.seconds;
-            minutes = minutes + timestamp.minutes;
+            minutes += time[0];
+            seconds += time[1];
         }
-
     });
 
     // Convert all to seconds and add them.
@@ -62,4 +49,5 @@ function PlayListDuration() {
     // Store final Time and insert into the appended list item;
     var Time = hh + "h:" + mm + "m:" + ss + "s";
     $('#duration').html("Duration <span>" + Time + "</span>");
+    console.log(Time);
 }
